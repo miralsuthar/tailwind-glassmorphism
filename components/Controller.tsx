0ac3e81467fb2bd255a35c7047b8c4f080eb8d30
Miral Suthar <57826091+miralsuthar@users.noreply.github.com>
2021-09-15 14:39:25 +0530

    add input for opacity

--- components/Controller.tsx
+++ components/Controller.tsx
@@ -1,5 +1,37 @@
 import React from 'react';
+import Slider from 'react-input-slider';
 
-export const Controller = () => {
-  return <div className="h-20 w-3/6 bg-white rounded-md bg-secondary"></div>;
+type Props = {
+  value: number;
+  maxValue: number;
+  minValue: number;
+  step: number;
+  onChange: any;
+};
+export const Controller = ({
+  value,
+  maxValue,
+  minValue,
+  step,
+  onChange,
+}: Props) => {
+  return (
+    <div className="h-20 w-3/6 bg-white rounded-md bg-secondary p-2">
+      <div>
+        <p className="text-white">Background Opacity</p>
+        <Slider
+          axis="x"
+          x={value}
+          xmax={maxValue}
+          xmin={minValue}
+          xstep={step}
+          onChange={onChange}
+        />
+      </div>
+
+      <div>
+        <p></p>
+      </div>
+    </div>
+  );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,23 @@
+import React, { useState } from 'react';
 import Image from 'next/image';
 import type { NextPage } from 'next';
 import { Glass } from '../components/Glass';
 import { Controller } from '../components/Controller';
 
 const Home: NextPage = () => {
+  const [opacity, setOpacity] = useState(0);
+
   return (
     <div className="h-screen w-screen flex flex-col justify-center items-center gap-5">
-      <Controller />
-      <Glass blur="xl" opacity="20" />
+      <Controller
+        value={opacity}
+        maxValue={100}
+        minValue={0}
+        step={10}
+        // @ts-ignore
+        onChange={({ x }) => setOpacity(x)}
+      />
+      <Glass blur="xl" opacity={opacity.toString()} />
     </div>
   );
 };
