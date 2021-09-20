75261bb59825aa8516cb661320903050cc2a7e71
Miral Suthar <57826091+miralsuthar@users.noreply.github.com>
2021-09-20 17:34:05 +0530

    add upload image button and add styles to it

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import React, { useState, useEffect } from 'react';
 import type { NextPage } from 'next';
 
@@ -34,6 +35,11 @@ const Home: NextPage = () => {
     { value: 'purple', label: 'Purple' },
   ];
 
+  const preview = (files: any) => {
+    const imageSrc = URL.createObjectURL(files[0]);
+    setImageUrl(imageSrc);
+  };
+
   return (
     <div className="h-screen w-screen flex flex-col justify-center items-center gap-5">
       <div className="flex gap-4">
@@ -60,6 +66,29 @@ const Home: NextPage = () => {
           // @ts-ignore
           selectChange={({ value }) => setColor(value)}
         />
+        <div className="overflow-hidden bg-secondary relative w-64 mt-4 mb-5 rounded-md">
+          <button className="z-0 cursor-pointer bg-blue hover:bg-blue-light text-white font-bold py-2 px-4 w-full inline-flex items-center">
+            <svg
+              fill="#FFF"
+              height="18"
+              viewBox="0 0 24 24"
+              width="18"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M0 0h24v24H0z" fill="none" />
+              <path d="M9 16h6v-6h4l-7-7-7 7h4zm-4 2h14v2H5z" />
+            </svg>
+            <span className="ml-2">Change Background</span>
+          </button>
+          <input
+            className="cursor-pointer absolute block z-10 top-0 left-0 w-full opacity-0 pin-r pin-t"
+            type="file"
+            accept="image/png, image/jpeg, image/jpg"
+            onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+              preview(event.target.files)
+            }
+          />
+        </div>
       </div>
 
       <Glass
