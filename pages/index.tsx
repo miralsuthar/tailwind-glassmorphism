aff3a786b2fe2b442f0610e989aa29cc007bbc56
Miral Suthar <57826091+miralsuthar@users.noreply.github.com>
2021-09-23 11:56:11 +0530

    add meta in head element in index

--- pages/index.tsx
+++ pages/index.tsx
@@ -43,7 +43,10 @@ const Home: NextPage = () => {
 
   return (
     <main>
-      <Meta />
+      <Head>
+        <Meta />
+      </Head>
+
       <div className="h-screen w-screen flex flex-col justify-center items-center gap-5">
         <h1 className="text-4xl text-white font-bold -mt-10 mb-10">
           Tailwindcss Glassmorphism Generator
